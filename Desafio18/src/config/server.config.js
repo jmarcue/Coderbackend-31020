db1dd001abff1d53fadd64c140bc3274a5642a81
Jaime Martinez <jmartinezcuevas@gmail.com>
2022-11-08 03:18:48 -0300

    commit

--- Desafio18/src/config/server.config.js
+++ Desafio18/src/config/server.config.js
@@ -9,6 +9,7 @@ const portDef = process.env.PORT || 8080;
 const { port } = minimist(process.argv.slice(2), { alias: { "p": "port" }, default: { "port":  portDef} });
 
 export const serverConfig = {
+  STORAGE: process.env.STORAGE,
   MONGO_ATLAS: process.env.MONGO_ATLAS || "",
   MONGO_LOCAL: process.env.MONGO_LOCAL || "",
   PORT: port
